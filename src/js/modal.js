64137b8b31a78c20b09b0b819586fe07bcd41926
Piotrek <138370309+Shunii06@users.noreply.github.com>
2024-01-29 01:13:15 +0100

    all modals - in progress

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn2: document.querySelector("[data-modal-open='2']"),
+    closeModalBtn2: document.querySelector("[data-modal-close='2']"),
+    modal2: document.querySelector("[data-modal='2']"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.closeModalBtn2.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal2.classList.toggle("is-hidden");
   }
 })();
